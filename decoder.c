8d5b87111fbe2cb2f734c9a8dd2080b1549304e7
Irwin <irwin1139@gmail.com>
2023-12-28 08:52:16 +0200

    Add biasing to C version of convolve

--- decoder.c
+++ decoder.c
@@ -10,7 +10,7 @@ void convolve_muladd (float *arr, int count, float kernel, float *arr_out)
 }
 
 __declspec(dllexport)
-void convolve_mc (float *arr, int in_channel_count, int array_count, float *kernels, float *arr_out)
+void convolve_mc (float *arr, int in_channel_count, int array_count, float *kernels, float *arr_out, float bias)
 {
     memset(arr_out, 0, array_count * sizeof(float));
 
@@ -19,26 +19,56 @@ void convolve_mc (float *arr, int in_channel_count, int array_count, float *kern
         int stride = i * array_count;
         convolve_muladd(arr + stride, array_count, kernels[i], arr_out);
     }
+
+    for (int i = 0; i < array_count; ++i)
+    {
+        arr_out[i] += bias;
+    }
+}
+
+__declspec(dllexport)
+void convolve_mc_mf_bias (float *arr, int in_channel_count, int array_count, float *kernels, int filter_count, float *arr_out, float *bias)
+{
+    for (int i = 0; i < filter_count; ++i)
+    {
+        int out_stride = i * array_count;
+        convolve_mc(arr, in_channel_count, array_count, kernels + i * in_channel_count, arr_out + out_stride, bias ? bias[i] : 0.0f);
+    }
 }
 
 __declspec(dllexport)
 void convolve_mc_mf (float *arr, int in_channel_count, int array_count, float *kernels, int filter_count, float *arr_out)
 {
-    for (int i = 0; i < filter_count; ++i)
-    {
-        int out_stride = i * array_count;
-        convolve_mc(arr, in_channel_count, array_count, kernels + i * in_channel_count, arr_out + out_stride);
-    }
+    convolve_mc_mf_bias(arr, in_channel_count, array_count, kernels, filter_count, arr_out, 0);
 }
 
 
+
 __declspec(dllexport)
-void convolve_mc_mf_batch (int batch, float *arr, int in_channel_count, int array_count, float *kernels, int filter_count, float *arr_out)
+void convolve_mc_mf_batch_bias (int batch, float *arr, int in_channel_count, int array_count, float *kernels, int filter_count, float *arr_out, float *bias)
 {
     for (int i = 0; i < batch; ++i)
     {
         int stride = i * in_channel_count * array_count;
         int out_stride = i * array_count * filter_count;
-        convolve_mc_mf(arr + stride, in_channel_count, array_count, kernels, filter_count, arr_out + out_stride);
+        convolve_mc_mf_bias(arr + stride, in_channel_count, array_count, kernels, filter_count, arr_out + out_stride, bias);
+    }
+}
+
+__declspec(dllexport)
+void convolve_mc_mf_batch (int batch, float *arr, int in_channel_count, int array_count, float *kernels, int filter_count, float *arr_out)
+{
+    convolve_mc_mf_batch_bias(batch, arr, in_channel_count, array_count, kernels, filter_count, arr_out, 0);
+}
+
+__declspec(dllexport)
+void relu_inplace (float *arr, int array_count)
+{
+    for (int i = 0; i < array_count; ++i)
+    {
+        if (arr[i] < 0.0f)
+        {
+            arr[i] = 0.0f;
+        }
     }
 }
