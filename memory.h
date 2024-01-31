34e8ae982bcf40aa6b98c87691a741fde3b53cac
Irwin <irwin1139@gmail.com>
2024-01-31 08:20:49 +0200

    Add disabled debug code to print asan shadow memory

--- memory.h
+++ memory.h
@@ -80,6 +80,25 @@ MemoryArena *DEBUG_getDebugArena();
 
 #include <sanitizer/asan_interface.h>
 
+
+#if 0
+static char *formatted_shadow_memory_print_str =
+"%0p: %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx\n";
+size_t shadow_memory_scale, shadow_memory_offset;
+
+void __declspec(no_sanitize_address)print_shadow_memory( void *address )
+{
+   uint8_t *shadow_memory = (uint8_t *)((size_t)address >> shadow_memory_scale) +
+      shadow_memory_offset;
+   fprintf( stderr, formatted_shadow_memory_print_str, address, shadow_memory[0],
+            shadow_memory[1], shadow_memory[2], shadow_memory[3], shadow_memory[4],
+            shadow_memory[5], shadow_memory[6], shadow_memory[7] );
+}
+#define PRINT_SHADOW_MEMORY(address)                                           \
+  fprintf(stderr, "Shadow Memory at " __FILE__ ":%d\n", __LINE__);                      \
+  print_shadow_memory(address);
+#endif
+
 static u8 debug_arena_buffer_2[Megabytes( 16 )];
 
 static MemoryArena DEBUG_debug_arena_2 = {0};
