ae94a3217cace9574d94517a6961960e06a0f6a4
Irwin <irwin1139@gmail.com>
2024-04-27 09:37:34 +0300

    Fix wrong size calculation for tensor slicing (actually indexing)

--- tensor.h
+++ tensor.h
@@ -133,8 +133,8 @@ static inline TestTensor tensor_slice_first_dim( TestTensor *tensor_to_slice, in
          result.dims = tensor_to_slice->dims + 1;
       }
 
-      result.size = tensor_to_slice->size - offset;
-      result.nbytes = tensor_to_slice->nbytes - (tensor_to_slice->nbytes / tensor_to_slice->size) * offset;
+      result.size = first_dimension_stride;
+      result.nbytes = first_dimension_stride * (tensor_to_slice->nbytes / tensor_to_slice->size);
    }
 
    return result;
