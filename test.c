b373929b706870c37022c6e33d55b6248b2b253c
Irwin <irwin1139@gmail.com>
2024-09-06 10:24:35 +0300

    Add V5 lstm test

--- test.c
+++ test.c
@@ -443,6 +443,65 @@ TestResult lstm_test_RED_1layer()
    return pass_lstm;
 }
 
+TestResult lstm_test_RED_v5()
+{
+   MemoryArena *debug_arena = DEBUG_getDebugArena();
+   TemporaryMemory mark = beginTemporaryMemory( debug_arena );
+
+   LoadTesttensorResult lstm_testdata = {0};
+
+   lstm_testdata = load_testtensor(debug_arena, "testdata\\untracked\\RED600_lstm_v5.testtensor" );
+
+   if (lstm_testdata.tensor_count == 0)
+   {
+      endTemporaryMemory( mark );
+      TestResult test_result = {0};
+      return test_result;
+   }
+
+
+   TestTensor *input = lstm_testdata.tensor_array + 0;
+
+   TestTensor *weights = lstm_testdata.tensor_array + 1;
+   TestTensor *biases = lstm_testdata.tensor_array + 2;
+
+   TestTensor *output_reference = lstm_testdata.tensor_array + 3;
+
+   Assert( input->ndim == 3 );
+   int batches = input->dims[0];
+   int seq_length = input->dims[1];
+   int input_size = input->dims[2];
+   int layer_count = weights->dims[0];
+
+   int batch_stride = seq_length * input_size;
+   int lstm_output_size = batch_stride * batches + (input_size * layer_count * 2);
+   // Assert(lstm_output_size == output_reference->size);
+
+   int hc_size = input_size * layer_count;
+   float *input_h_array = pushArray( debug_arena, hc_size, float );
+   float *input_c_array = pushArray( debug_arena, hc_size, float );
+   float *output_single_batch = pushArray( debug_arena, lstm_output_size, float );
+   // float *output_combined = pushArray( debug_arena, batches * seq_length * input_size, float );
+
+   lstm_seq( debug_arena, input->data,
+             seq_length * batches,
+             input_size,
+             input_h_array,
+             input_c_array,
+             weights->data,
+             biases->data,
+             output_single_batch,
+             layer_count
+   );
+   // print_array(output_single_batch, lstm_output_size);
+
+   TestResult pass_lstm = all_close( output_reference->data, output_single_batch, batch_stride * batches, 1e-04f );
+
+   endTemporaryMemory( mark );
+
+   return pass_lstm;
+}
+
 TestResult dw_conv_129_test()
 {
    MemoryArena *debug_arena = DEBUG_getDebugArena();
@@ -1673,6 +1732,7 @@ TestFunctionDescription test_function_descriptions[] =
    TEST_FUNCTION_DESCRIPTION(lstm_test_RED),
    TEST_FUNCTION_DESCRIPTION(lstm_test_RED_new),
    TEST_FUNCTION_DESCRIPTION(lstm_test_RED_1layer),
+   TEST_FUNCTION_DESCRIPTION(lstm_test_RED_v5),
 };
 
 // int main(int argc, char *argv[])
